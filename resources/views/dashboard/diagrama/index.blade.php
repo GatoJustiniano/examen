18fa75aa23882bdf26b0c90395015992d8a273ef
Andres Justiniano <andres95liverpool@gmail.com>
2021-11-16 03:48:05 -0400

    mejora en la presentación de diagrama

--- resources/views/dashboard/diagrama/index.blade.php
+++ resources/views/dashboard/diagrama/index.blade.php
@@ -1,10 +1,27 @@
 <html>
+    <head>
+        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    </head>
 
-    <body>
+    <body class="container">
         <script src="{{ asset('js/diagram-editor.js') }}"></script>
-        <img onclick="DiagramEditor.editElement(this, true, 'min', null, ['dark=0']);"
-            src=""
-            style="cursor:pointer;" alt="comenzar">
+
+        <div class="row mt-5">
+            <div class="col-6">
+                <img src="img\modo-dia.png" 
+                width="100%" height="250">
+                <img onclick="DiagramEditor.editElement(this, true, 'min', null, ['dark=0','toolbar=0','sync=manual']);"
+                    src=""
+                    style="cursor:pointer;" alt="Modo claro">
+            </div>
+            <div class="col-6">
+                <img src="img\modo-oscuro.png" 
+                width="100%" height="250">
+                <img onclick="DiagramEditor.editElement(this, true, 'min', null, ['dark=1','toolbar=0','sync=manual']);"
+                    src=""
+                    style="cursor:pointer;" alt="Modo oscuro">
+            </div>
+        </div>
     </body>
 
 </html>
