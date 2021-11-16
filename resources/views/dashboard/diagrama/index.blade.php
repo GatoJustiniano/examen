77a858217f9818163cd983ad08c1873a2cae3d04
Andres Justiniano <andres95liverpool@gmail.com>
2021-11-16 17:38:50 -0400

    add las imagenes correspondientes de minimal y skecth

--- resources/views/dashboard/diagrama/index.blade.php
+++ resources/views/dashboard/diagrama/index.blade.php
@@ -1,45 +1,46 @@
 <html>
     <head>
+        <title>Diagrama Modelo C4</title>
         <link rel="stylesheet" href="{{ asset("css/app.css") }}">
     </head>
 
-    <body class="container" background="black">
+    <body class="container" style="background-color:#e5e5e5;">
         <script src="{{ asset('js/diagram-editor.js') }}"></script>
 
         <div class="row mt-2">
             <div class="col-2">
-                Escoge la interfaz de usuario Minimal en sus dos modos:
+                Escoge la interfaz de usuario <b>Minimal</b> en sus dos modos:
             </div>
             <div class="col">
-                <img src="img\modo-dia.png" 
+                <img src="img\minimal-modo-claro.png" 
                 width="100%" height="250">
                 <img onclick="DiagramEditor.editElement(this, true, 'min', null, ['dark=0','toolbar=0','sync=manual']);"
                     src=""
                     style="cursor:pointer;" alt="Modo claro">
             </div>
             <div class="col">
-                <img src="img\modo-oscuro.png" 
+                <img src="img\minimal-modo-oscuro.png" 
                 width="100%" height="250">
                 <img onclick="DiagramEditor.editElement(this, true, 'min', null, ['dark=1','toolbar=0','sync=manual']);"
                     src=""
                     style="cursor:pointer;" alt="Modo oscuro">
             </div>
         </div>
-        <div class="row mt-5">
+        <div class="row mt-4">
             <div class="col-2">
-                Escoge la interfaz de usuario Sketch en sus dos modos:
+                Escoge la interfaz de usuario <b>Sketch</b>  en sus dos modos:
             </div>
             <div class="col">
-                <img src="img\modo-dia.png" 
+                <img src="img\sketch-modo-claro.png" 
                 width="100%" height="250">
-                <img onclick="DiagramEditor.editElement(this, true, 'sketch', null, ['dark=0','toolbar=0','sync=manual']);"
+                <img onclick="DiagramEditor.editElement(this, true, 'sketch', null, ['dark=0']);"
                     src=""
                     style="cursor:pointer;" alt="Modo claro">
             </div>
             <div class="col">
-                <img src="img\modo-oscuro.png" 
+                <img src="img\sketch-modo-oscuro.png" 
                 width="100%" height="250">
-                <img onclick="DiagramEditor.editElement(this, true, 'sketch', null, ['dark=1','toolbar=0','sync=manual']);"
+                <img onclick="DiagramEditor.editElement(this, true, 'sketch', null, ['dark=1']);"
                     src=""
                     style="cursor:pointer;" alt="Modo oscuro">
             </div>
