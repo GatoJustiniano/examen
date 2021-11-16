42dd9007a682d5b98f0247184dd23024fe56b9b6
Andres Justiniano <andres95liverpool@gmail.com>
2021-11-16 03:56:02 -0400

    add UI Min y sketch

--- resources/views/dashboard/diagrama/index.blade.php
+++ resources/views/dashboard/diagrama/index.blade.php
@@ -3,21 +3,43 @@
         <link rel="stylesheet" href="{{ asset("css/app.css") }}">
     </head>
 
-    <body class="container">
+    <body class="container" background="black">
         <script src="{{ asset('js/diagram-editor.js') }}"></script>
 
-        <div class="row mt-5">
-            <div class="col-6">
+        <div class="row mt-2">
+            <div class="col-2">
+                Escoge la interfaz de usuario Minimal en sus dos modos:
+            </div>
+            <div class="col">
                 <img src="img\modo-dia.png" 
                 width="100%" height="250">
                 <img onclick="DiagramEditor.editElement(this, true, 'min', null, ['dark=0','toolbar=0','sync=manual']);"
                     src=""
                     style="cursor:pointer;" alt="Modo claro">
             </div>
-            <div class="col-6">
+            <div class="col">
                 <img src="img\modo-oscuro.png" 
                 width="100%" height="250">
                 <img onclick="DiagramEditor.editElement(this, true, 'min', null, ['dark=1','toolbar=0','sync=manual']);"
+                    src=""
+                    style="cursor:pointer;" alt="Modo oscuro">
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-2">
+                Escoge la interfaz de usuario Sketch en sus dos modos:
+            </div>
+            <div class="col">
+                <img src="img\modo-dia.png" 
+                width="100%" height="250">
+                <img onclick="DiagramEditor.editElement(this, true, 'sketch', null, ['dark=0','toolbar=0','sync=manual']);"
+                    src=""
+                    style="cursor:pointer;" alt="Modo claro">
+            </div>
+            <div class="col">
+                <img src="img\modo-oscuro.png" 
+                width="100%" height="250">
+                <img onclick="DiagramEditor.editElement(this, true, 'sketch', null, ['dark=1','toolbar=0','sync=manual']);"
                     src=""
                     style="cursor:pointer;" alt="Modo oscuro">
             </div>
